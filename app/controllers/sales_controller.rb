d7f5fa1b2447671336510e22638f709fc094c800
Puneet <puneetgupta18190@gmail.com>
2017-07-29 23:13:54 +0530

    fixed issues

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,42 +7,44 @@ class SalesController < ApplicationController
         # UserMailer.welcome_email(@user).deliver!
       else
         @errors = @user.errors.full_messages
-        render :template => "services/show"
+        redirect_to "/services/#{params[:service_id]}"
+        #render :template => "services/show"
       end
 
     end
 
-    @sale = @user.sales.new(sales_params)
-    @sale.save(validate: false)
+    if @errors.blank?
+      @sale = @user.sales.new(sales_params)
+      @sale.save
 
-    unless params[:total_amount].to_i == 0
-      if params[:payment_method] == "stripe"
-        Stripe.api_key = Rails.application.secrets.stripe_secret_key
-        amt = sprintf('%.2f', params[:total_amount])
-        plan = "plan_#{amt}"
-        card_token = Stripe::Token.create( :card => { :name => params[:card_name], :number => params[:card_number], :exp_month => params[:exp_month], :exp_year => params[:exp_year], :cvc => params[:card_cvv] })
-        customer_params = {:card => card_token[:id], :plan => plan, :email => current_user.email}
+      unless params[:total_amount].to_i == 0
+        if params[:payment_method] == "stripe"
+          Stripe.api_key = Rails.application.secrets.stripe_secret_key
+          amt = sprintf('%.2f', params[:total_amount])
+          plan = "plan_#{amt}"
+          card_token = Stripe::Token.create( :card => { :name => params[:card_name], :number => params[:card_number], :exp_month => params[:exp_month], :exp_year => params[:exp_year], :cvc => params[:card_cvv] })
+          customer_params = {:card => card_token[:id], :plan => plan, :email => current_user.email}
 
-        stripe_customer = Stripe::Customer.create(customer_params) 
-        message = "Payment Successful"
-        redirect_to root_url, notice: message
-      end
-    
-      if params[:payment_method] == "paypal"
-        values = {
-          business: @user.email,
-          cmd: '_xclick',
-          upload: 1,
-          notify_url: root_url,
-          amount: params[:total_amount],
-          quantity: '1',
-          return: root_url
-        }
+          stripe_customer = Stripe::Customer.create(customer_params) 
+          message = "Payment Successful"
+          redirect_to root_url, notice: message
+        end
+      
+        if params[:payment_method] == "paypal"
+          values = {
+            business: @user.email,
+            cmd: '_xclick',
+            upload: 1,
+            notify_url: root_url,
+            amount: params[:total_amount],
+            quantity: '1',
+            return: root_url
+          }
 
-        redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+          redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+        end
       end
     end
-
   end
 
   def show
@@ -65,6 +67,6 @@ class SalesController < ApplicationController
     # if params[:note]
     #   params[:note] = params[:note].join
     # end
-    params.permit(:zip, :address, :state, :note, :amount, :service_id)
+    params.permit(:zip, :address, :state, :note, :amount, :service_id, :equipment_id)
   end
 end
