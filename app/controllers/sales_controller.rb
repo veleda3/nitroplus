804f05e54a36eb9c3259d9b7aaa2a82c1ce57962
Puneet <puneetgupta18190@gmail.com>
2017-07-29 15:02:27 +0530

    Stripe and Paypal Integration

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -13,7 +13,35 @@ class SalesController < ApplicationController
     end
 
     @sale = @user.sales.new(sales_params)
+    @sale.save(validate: false)
+
+    unless params[:total_amount].to_i == 0
+      if params[:payment_method] == "stripe"
+        Stripe.api_key = Rails.application.secrets.stripe_secret_key
+        amt = sprintf('%.2f', params[:total_amount])
+        plan = "plan_#{amt}"
+        card_token = Stripe::Token.create( :card => { :name => params[:card_name], :number => params[:card_number], :exp_month => params[:exp_month], :exp_year => params[:exp_year], :cvc => params[:card_cvv] })
+        customer_params = {:card => card_token[:id], :plan => plan, :email => current_user.email}
+
+        stripe_customer = Stripe::Customer.create(customer_params) 
+        message = "Payment Successful"
+        redirect_to root_url, notice: message
+      end
     
+      if params[:payment_method] == "paypal"
+        values = {
+          business: @user.email,
+          cmd: '_xclick',
+          upload: 1,
+          notify_url: root_url,
+          amount: params[:total_amount],
+          quantity: '1',
+          return: root_url
+        }
+
+        redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+      end
+    end
 
   end
 
@@ -23,20 +51,20 @@ class SalesController < ApplicationController
   private
 
   def user_params
-    params[:first_name] = params[:first_name].join
-    params[:last_name] = params[:last_name].join
-    params[:email] = params[:email].join
-    params[:password] = params[:password].join
+    # params[:first_name] = params[:first_name].join
+    # params[:last_name] = params[:last_name].join
+    # params[:email] = params[:email].join
+    # params[:password] = params[:password].join
     params.permit(:first_name, :last_name, :email, :password)
   end
 
   def sales_params
-    params[:zip] = params[:zip].join
-    params[:address] = params[:address].join
-    params[:state] = params[:state].join
-    if params[:note]
-      params[:note] = params[:note].join
-    end
+    # params[:zip] = params[:zip].join
+    # params[:address] = params[:address].join
+    # params[:state] = params[:state].join
+    # if params[:note]
+    #   params[:note] = params[:note].join
+    # end
     params.permit(:zip, :address, :state, :note, :amount, :service_id)
   end
 end
