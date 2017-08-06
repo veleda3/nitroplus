d86a154c30f62e7f162daa157bfa4e4971c518b8
a17100262 <maueezahmed@gmail.com>
2017-08-06 08:18:32 +0000

    Change in user_mailer_controller

--- app/controllers/user_mailer_controller.rb
+++ app/controllers/user_mailer_controller.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
-    mail(:to => "#{user.name} <#{user.email}, :subject => "Registration Confirmation for Awesome App)
- end
+    mail(:to => "#{user.name} <#{user.email}", :subject => "Registration Confirmation for Awesome App")
+  end
 
 end
